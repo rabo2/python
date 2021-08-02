cd9ccc4084864e302a1c24cab557e46f061d3606
rabo2 <81668709+rabo2@users.noreply.github.com>
2021-08-02 21:38:24 +0900

    Add files via upload

---
+++ day02/mydef02.py
@@ -0,0 +1,4 @@
+def mathPi():
+    return 3.141592
+
+print(mathPi())
---
+++ day02/mydef03.py
@@ -0,0 +1,14 @@
+def showDan(dan):
+    for i in range(1,10):
+        print(str(dan) + "*1="(dan+1))
+        print(str(dan) + "*2="(dan+1))
+        print(str(dan) + "*3="(dan+1))
+        print(str(dan) + "*4="(dan+1))
+        print(str(dan) + "*5="(dan+1))
+        print(str(dan) + "*6="(dan+1))
+        print(str(dan) + "*7"(dan+1))
+        print(str(dan) + "*8="(dan+1))
+        print(str(dan) + "*9="(dan+1))
+
+showDan(2)
+
