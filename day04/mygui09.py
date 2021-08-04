528211727fa13590d0a7f7bc26386946694c913e
rabo2 <81668709+rabo2@users.noreply.github.com>
2021-08-04 18:53:58 +0900

    Add files via upload

--- day04/mygui09.py
+++ day04/mygui09.py
@@ -11,67 +11,24 @@ class WindowClass(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("my form")
-        self.pb1.clicked.connect(self.pbClicked1)
-        self.pb2.clicked.connect(self.pbClicked2)
-        self.pb3.clicked.connect(self.pbClicked3)
-        self.pb4.clicked.connect(self.pbClicked4)
-        self.pb5.clicked.connect(self.pbClicked5)
-        self.pb6.clicked.connect(self.pbClicked6)
-        self.pb7.clicked.connect(self.pbClicked7)
-        self.pb8.clicked.connect(self.pbClicked8)
-        self.pb9.clicked.connect(self.pbClicked9)
-        self.pb0.clicked.connect(self.pbClicked0)
+        self.pb1.clicked.connect(self.pbClicked)
+        self.pb2.clicked.connect(self.pbClicked)
+        self.pb3.clicked.connect(self.pbClicked)
+        self.pb4.clicked.connect(self.pbClicked)
+        self.pb5.clicked.connect(self.pbClicked)
+        self.pb6.clicked.connect(self.pbClicked)
+        self.pb7.clicked.connect(self.pbClicked)
+        self.pb8.clicked.connect(self.pbClicked)
+        self.pb9.clicked.connect(self.pbClicked)
+        self.pb0.clicked.connect(self.pbClicked)
         self.pbCall.clicked.connect(self.pbClickedCall)
         
         
-    def pbClicked1(self):
-        str = self.le.text()+"1"
+    def pbClicked(self):
+        num = self.sender().text()
+        str = self.le.text()+num
         self.le.setText(str)
         
-        
-    def pbClicked2(self):
-        str = self.le.text()+"2"
-        self.le.setText(str)
-        
-        
-    def pbClicked3(self):
-        str = self.le.text()+"3"
-        self.le.setText(str)
-        
-        
-    def pbClicked4(self):
-        str = self.le.text()+"4"
-        self.le.setText(str)
-        
-        
-    def pbClicked5(self):
-        str = self.le.text()+"5"
-        self.le.setText(str)
-        
-        
-    def pbClicked6(self):
-        str = self.le.text()+"6"
-        self.le.setText(str)
-        
-        
-    def pbClicked7(self):
-        str = self.le.text()+"7"
-        self.le.setText(str)
-        
-        
-    def pbClicked8(self):
-        str = self.le.text()+"8"
-        self.le.setText(str)
-        
-        
-    def pbClicked9(self):
-        str = self.le.text()+"9"
-        self.le.setText(str)
-        
-        
-    def pbClicked0(self):
-        str = self.le.text()+"0"
-        self.le.setText(str)
     
     def pbClickedCall(self, event):
         reply = QMessageBox.question(self, 'Message', 'Calling..'+self.le.text(),QMessageBox.No) 
