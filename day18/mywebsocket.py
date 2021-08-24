37837c5452b83904b8625223e3344bea2eeda176
rabo2 <81668709+rabo2@users.noreply.github.com>
2021-08-24 21:22:01 +0900

    Add files via upload

---
+++ day18/mywebsocket.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+app.config['SECRET_KEY'] = '1234'
+socketio = SocketIO(app)
+
+@app.route('/tetris')
+def sessions():
+    return render_template('tetris.html')
+
+@app.route('/tetris_network')
+def sessions_network():
+    return render_template('tetris_network.html')
+
+def myreceive(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('to_server')
+def to_server(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('to_client', json, callback=myreceive)
+
+if __name__ == '__main__':
+    socketio.run(app, host = "0.0.0.0",port=9999)
